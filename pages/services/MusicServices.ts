eda05d696ac3deb80786b9c69cf864e9726d0e23
Ricardo <ricardolimadelemos.trab@gmail.com>
2023-12-10 13:00:09 -0300

    Atualizar EndPoint de músicas
    Nesse commit foi feita a retirada de comentários no  service e adição de trycath com mensagens de erros no endpoint.

--- pages/services/MusicServices.ts
+++ pages/services/MusicServices.ts
@@ -1,15 +1,15 @@
-import { MusicasModel } from '../../models/musicaSchema';
+import { MusicasModel } from '../../models/musicaSchema'
+import type { NextApiResponse } from 'next'
+import type { RespostaPadraoMsg } from '../../types/respostaPadrao'
 
 type MusicParameter = {
-  id: string;
-};
+  id: string
+}
 
 export const FindmusicById = async ({ id }: MusicParameter) => {
-  const musicasEncontradas = await MusicasModel.findById(id); // Retorna a música pelo ID
-
-
-  if (!musicasEncontradas) {
-    return null;
+  const musicasEncontradas = await MusicasModel.findById(id)
+  if (!musicasEncontradas || musicasEncontradas === null) {
+    return
   }
 
   return {
@@ -17,13 +17,15 @@ export const FindmusicById = async ({ id }: MusicParameter) => {
     url: musicasEncontradas.url,
     _id: musicasEncontradas._id,
     descricao: musicasEncontradas.descricao
-  };
-};
-
-export const FindAllMusic = async () => {
-  const musicasEncontradas = await MusicasModel.find(); // Retorna todas as músicas
-  if (!musicasEncontradas || musicasEncontradas === null  ) {
-    return { erro: 'Música não encontrada' };
   }
-  return musicasEncontradas;
-};
+}
+
+export const FindAllMusic = async (
+  res: NextApiResponse<RespostaPadraoMsg | any>
+) => {
+  const musicasEncontradas = await MusicasModel.find()
+  if (!musicasEncontradas || musicasEncontradas.length === 0) {
+    return res.status(400).json({ erro: 'Música não encontrada' })
+  }
+  return musicasEncontradas
+}
